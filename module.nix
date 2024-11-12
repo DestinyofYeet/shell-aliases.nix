d8d2ee3e1db66a4f3c5fc5aeece1bd0e41ea7c85
DestinyofYeet <olebend@gmail.com>
2024-11-12 11:21:19 +0100

    make module more generic
    Makes the module more generic by building options with a list of shells,
    making it easier to expand it in the future

--- module.nix
+++ module.nix
@@ -13,24 +13,37 @@ let
 
   filter-null = shell:
     filterAttrs (name: value: value.${shell} != null) cfg.aliases;
-  mkShellAliasShell = shell: lib.mkIf cfg."enable${(toUpper (substring 0 1 shell)) + (substring 1 (stringLength shell) shell)}" (mapAttrs (name: value: (value.${shell})) (filter-null shell));
+
+  mkFirstCharUpper = string:
+    (toUpper (substring 0 1 string)) + (substring 1 (stringLength string) string);
+
+  mkShellAliasShell = shell: lib.mkIf cfg."enable${mkFirstCharUpper shell}" (mapAttrs (name: value: (value.${shell})) (filter-null shell));
+
+  mkShellOption = shell: mkOption {
+    type = types.bool;
+    default = cfg.enable;
+  };
+
+  mkShells = shells: listToAttrs (map (name: { name = "enable${mkFirstCharUpper name}"; value = mkShellOption name; }) shells);
+
+  mkShellSubmoduleOption = shells: submoduleName: listToAttrs (map (name: { inherit name; value = mkOption {
+    type = types.nullOr types.str;
+    description = "The override value of ${name}";
+    default = cfg.aliases.${submoduleName}.default;
+  };}));
+
+  shellAliasShells = [
+    "bash"
+    "zsh"
+  ];
+
+  shells = [
+    "nushell"
+  ] ++ shellAliasShells;
 in {
   options = {
     programs.shell-aliases = {
       enable = mkEnableOption " shell-aliases";
-
-      enableNushell = mkOption {
-        type = types.bool;
-        default = cfg.enable;
-      };
-      enableBash = mkOption {
-        type = types.bool;
-        default = cfg.enable;
-      };
-      enableZsh = mkOption {
-        type = types.bool;
-        default = cfg.enable;
-      };
 
       aliases = mkOption {
         type = types.attrsOf (types.either (types.submodule ({name, ...}: {
@@ -40,41 +53,34 @@ in {
               description = "The alias applied to all shells if not overridden";
             };
 
-            nushell = mkOption {
-              type = types.nullOr types.str;
-              description = "The override value of nushell";
-              default = cfg.aliases.${name}.default;
-            };
+            # shellName = mkOption {
+            #   type = types.nullOr types.str;
+            #   description = "The override value of zsh";
+            #   default = cfg.aliases.${name}.default;
+            # };
 
-            bash = mkOption {
-              type = types.nullOr types.str;
-              description = "The override value of bash";
-              default = cfg.aliases.${name}.default;
-            };
-
-            zsh = mkOption {
-              type = types.nullOr types.str;
-              description = "The override value of zsh";
-              default = cfg.aliases.${name}.default;
-            };
-          };
+            # the below merge achieves the option above, but for every shell
+          } // listToAttrs (map (shellName: { name = shellName; value = mkOption {
+            type = types.nullOr types.str;
+            description = "The override value of ${shellName}";
+            default = cfg.aliases.${name}.default;
+          };}) shells);
         })) types.str);
 
         apply = old:
           mapAttrs (name: value: if !isString value then value else {
             default = value;
-            nushell = value;
-            bash = value;
-            zsh = value;
-          }) old;
+
+            # generates shellName = value;
+          } // (listToAttrs (map (name: { inherit name value; }) shells))) old;
       };
-    };
+    } // mkShells shells;
   };
 
   config = mkIf cfg.enable {
     programs = {
       nushell.configFile.text = lib.mkIf cfg.enableNushell (concatStringsSep "\n" (mapAttrsToList (name: value: "alias ${name} = ${value.nushell}") (filter-null "nushell")));
   
-    } // listToAttrs (map (name: { inherit name; value = { shellAliases = mkShellAliasShell name;};}) [ "zsh" "bash" ]);
+    } // listToAttrs (map (name: { inherit name; value = { shellAliases = mkShellAliasShell name;};}) shellAliasShells);
   };
 }
